da907592d6270da7fc4c59682aab41d8c67ba645
manasiideo <manasideo15.com>
2026-01-25 12:28:35 +0530

    Operators in Python

---
+++ PythonLecture.py/Learning_Python.py/Variables_and_DataType.py.py
@@ -0,0 +1,27 @@
+# Python -> Translator (compiler/Interpreter) -> Machine Language (0,1)
+# 1 = high voltage
+# 0 = low volateg
+# Programming Language -> PYTHON (High Level Language)
+# High Level Language -> Translator(works as) - > Low Level Language
+# PYTHON -> Interpreter (NOT - compiler)
+# PYTHON = simple Programming Language
+#         free and open source
+#         High Level Language
+#         Developed by - GUIDO van Rossum
+#         Portable Language - can be same in different operating system (windows, mac, linus, etc)
+# Visual Studio Code -> Digital Notebook Code Editor
+print("Hello WOrld") #FirstProgram 
+#Always use "print" and not "Print"
+# "print" = python's function
+# INPUT -> CODE -> OUTPUT
+# Python Chatacters:
+#  1:A to Z and a to z
+#  2:Deigits: 0-9
+#  3: Symbols: -, +, *, / (keyboard keys)
+#  4: WhiteSpaces: 
+#                  i: Blank WhiteSpaces
+#                 ii: Tab
+#                iii: Carriage return
+#                 iv: Newline 
+#                  v: Formfeed
+#  5: python can process all "ASCII" and "Unicode characters" as part of data or literals
---
+++ PythonLecture.py/neew.py
@@ -0,0 +1,5 @@
+str1 = "this is a string." #notneeded
+print (str1)
+str2 = "Hello String"
+print(str1 + "\t" + str2)
+ptint("""this is a string. + "\t" + Hello String""")
